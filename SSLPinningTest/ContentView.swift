65cefa04cc0205b5d75f41e5c9a767fc35819375
Zhou Hao <zhou.hao.27@gmail.com>
2023-07-25 12:30:03 +0800

    Initial Commit

---
+++ SSLPinningTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SSLPinningTest
+//
+//  Created by Zhou Hao on 25/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SSLPinningTest/SSLPinningTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSLPinningTestApp.swift
+//  SSLPinningTest
+//
+//  Created by Zhou Hao on 25/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SSLPinningTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
